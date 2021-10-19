0565789a41949bdeceb4a855a01c849521e0e497
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-10-19 12:12:52 +0530

    built auth guard and added routes

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from '../services/account.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private _accountService: AccountService,
+    private _router: Router
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean {
+    const currentUser = this._accountService.currentUserValue;
+    const isLoggedIn = currentUser && currentUser?.token;
+
+    if (isLoggedIn) {
+      return true;
+    }
+
+    this._router.navigate(['/']);
+    return false;
   }
-  
 }
