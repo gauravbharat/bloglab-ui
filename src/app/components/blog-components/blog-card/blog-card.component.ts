59037a42d57df9931eaebb37dec024c4fcfeca2a
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-10-20 16:04:26 +0530

    created blog card component

--- src/app/components/blog-components/blog-card/blog-card.component.ts
+++ src/app/components/blog-components/blog-card/blog-card.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Blog } from 'src/app/models/blog/blog.model';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.css']
+  styleUrls: ['./blog-card.component.css'],
 })
 export class BlogCardComponent implements OnInit {
+  @Input() blog!: Blog;
 
-  constructor() { }
+  blogPhotoUrl!: string;
+
+  constructor(private _router: Router, private _photoService: PhotoService) {}
 
   ngOnInit(): void {
+    if (!!this.blog?.photoId) {
+      this._photoService.get(this.blog.photoId).subscribe(
+        (photo) => {
+          if (!!photo) this.blogPhotoUrl = photo.imageUrl;
+        },
+        (err) =>
+          console.log('BlogCardComponent _photoService.get API', {
+            blog: this.blog,
+            err,
+          })
+      );
+    }
   }
 
+  readMore(blogId: number): void {
+    this._router.navigate([`/blogs/${this.blog.blogId}`]);
+  }
 }
