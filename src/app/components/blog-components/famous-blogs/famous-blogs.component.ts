0398fb1b68fd5e914ae97fff92271d8e1eb56ac5
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-10-20 17:10:35 +0530

    built famous blogs component

--- src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
+++ src/app/components/blog-components/famous-blogs/famous-blogs.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Blog } from 'src/app/models/blog/blog.model';
+import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
   selector: 'app-famous-blogs',
   templateUrl: './famous-blogs.component.html',
-  styleUrls: ['./famous-blogs.component.css']
+  styleUrls: ['./famous-blogs.component.css'],
 })
 export class FamousBlogsComponent implements OnInit {
+  famousBlogs: Blog[] = [];
 
-  constructor() { }
+  constructor(private _blogService: BlogService) {}
 
   ngOnInit(): void {
+    this._blogService.getMostFamous().subscribe(
+      (famousBlogs) => (this.famousBlogs = famousBlogs),
+      (err) => console.log('_blogService.getMostFamous API', { err })
+    );
   }
-
 }
